2415efd1f7168e439579c5291a8b39e8769eb026
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-06-30 18:12:43 +0300

    upload files on dropbox

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
 const express = require("express");
-const app = express()
-const bodyParser = require("body-parser")
-const PORT = process.env.PORT || 3000
-const mongoose = require("mongoose")
-const router = require("./src/Routes/routes")
-const cors = require("cors")
-const fileUpload = require('express-fileupload');
-
-const dropboxV2Api = require('dropbox-v2-api');
-
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const router = require("./src/Routes/routes");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const dropboxV2Api = require("dropbox-v2-api");
+const controllers = require('./src/Controllers/controllers')
 const dropbox = dropboxV2Api.authenticate({
-	token: 'oXHBknHRYiAAAAAAAAAAEEMkNZmLVkldMsll6K2DNp6ntH-D_4XRUutat2jBAs5S'
+  token: "oXHBknHRYiAAAAAAAAAARuNFVS7_uMPA7BMImzSj95RDFVTbzkM2ApmZP_Ce2aju",
 });
-console.log('----------------------------------');
+console.log("----------------------------------");
 
-const downloadStream = dropbox({
-    resource: 'files/download',
-    parameters: { path: '/source/file/path' }				
-});
- 
-const uploadStream = dropbox({
-    resource: 'files/upload',
-    parameters: { path: '/home/user/Desktop/backend/backend/src/dropbox/dropbox.js' }				
+// dropbox({
+//     resource: 'files/download',
+//     parameters: {
+//         path: '/user_5efb2be7bb9a2111adde0e05/node-v14.2.0-linux-x64.tar.xz'
+//     }
+// }, (err, result, response) => {
+//     //download completed
+// })
+// .pipe(fs.createWriteStream('/home/user/Downloads/node-v14.2.0-linux-x64.tar.xz'));
+// // use session ref to call API, i.e.:
+
+exports.uploadStream1 = (name, id) => {
+  const uploadStream = dropbox(
+    {
+      resource: "files/upload",
+      parameters: { path: `/user_${id}/${name}`},
+      'mode': 'add',
+      'autorename': true,
+      'mute': false,
+      'strict_conflict': false,
+      symlink_info:true
+    },
+    (err, result, response) => {
+    //   console.log("======================", result);
+
+      fs.rmdirSync(`./upload/user_${id}`, { recursive: true });
+    }
+  );
+  fs.createReadStream(
+    `/home/user/Desktop/backend/backend/upload/user_${id}/${name}`
+  ).pipe(uploadStream);
+};
+dropbox({
+    resource: 'deprecated/create_shared_link',
+    parameters: {
+        'path': '/user_5ef5e1de21d237698be58c24/t45p9fh0x-u01021k8q9g-cddc98106831-512 (1).png',
+        'short_url': false
+    }
 }, (err, result, response) => {
-    console.log('asdasdasdasdasdasd', response)
-    //upload finished
+    console.log(result.url)
 });
- 
-downloadStream.pipe(uploadStream);
 
-app.use(cors())
+// downloadStream.pipe(uploadStream);
+
+app.use(cors());
 app.use(fileUpload());
-require('dotenv').config();
+require("dotenv").config();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose
-    .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-    .then(() => {
-
-        console.log('Database is connected successfully');
-    })
-    .catch((err) => {
-        console.log('Error with connecting to database');
-    });
-app.use(bodyParser.json())
-app.use('/user', router)
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database is connected successfully");
+  })
+  .catch((err) => {
+    console.log("Error with connecting to database");
+  });
+app.use(bodyParser.json());
+app.use("/user", router);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
--- src/Controllers/controllers.js
+++ src/Controllers/controllers.js
@@ -1,6 +1,7 @@
 const userModel = require("../Models/users");
 // const dropbox = require('../dropbox/dropbox')
-
+const index = require("../../index");
+const fs = require('fs')
 exports.showUserInfo = async (req, res, next) => {
   const { id } = req.params;
   const user = await userModel.findOne({ _id: id });
@@ -14,7 +15,8 @@ exports.showUserInfo = async (req, res, next) => {
   console.log("showUserInfo");
 };
 
-exports.updateUserInfo = async (req, res, next) => {
+exports.updateUserInfo = async (req, res, next, url) => {
+  console.log("NSAAAAAAAAAAMEEEEEEEEEEEEEEEe================",url);
   const { firstName, lastName, age, gender, aboutYourself, avatar } = req.body;
   const { id } = req.params;
   const user = await userModel.findOneAndUpdate(
@@ -45,42 +47,51 @@ exports.showFiles = async (req, res, next) => {
   console.log("showFiles");
 };
 exports.postUserAvatar = async (req, res, next) => {
- 
   const file = req.body.file;
-  console.log('-------------------------------file',file)
-  const { id } = req.params;
-  const user = await userModel.findOneAndUpdate({ _id: id }, { avatar: file },{new: true});
-  if (!user) {
-    return res.status(400).json({ msg: "Not found" });
-  }
-
-};
-
-exports.postUnloadFile = async (req, res, next) => {
-
-  const file = req.files.file;
+  console.log("-------------------------------file", file);
   const { id } = req.params;
   const user = await userModel.findOneAndUpdate(
     { _id: id },
-    { $push: { files: file } },
-    { returnOriginal: false }
+    { avatar: file },
+    { new: true }
   );
+  if (!user) {
+    return res.status(400).json({ msg: "Not found" });
+  }
+};
+
+exports.postUnloadFile = async (req, res, next) => {
+  console.log("NSAAAAAAAAAAMEEEEEEEEEEEEEEEe================");
+  const file = req.files.file;
+  const { id } = req.params;
+  
+  fs.mkdirSync(`./upload/user_${id}`,{recursive:true})
+ 
+  const user = await userModel
+    .findOneAndUpdate
+    // { _id: id },
+    // { $push: { files: file } },
+    // { returnOriginal: false }
+    ();
   if (!user) {
     return res.status(404).json({
       message: "Not found",
     });
   }
   file.mv(
-    `/home/user/Desktop/backend/backend/public/files/${file.name}`,
+    `/home/user/Desktop/backend/backend/upload/user_${id}/${file.name}`,
     (err) => {
       if (err) {
         console.error(err);
         return res.status(500).send(err);
       }
-
-      res.json({ fileName: file.name, filePath: `/files/${file.name}` });
+  
+      // res.json({ fileName: file.name, filePath: `/files/${file.name}` });
     }
+    
   );
+  index.uploadStream1(file.name, id);
+  
 };
 
 exports.getDownloadFile = async (req, res, next) => {
